e5f33e64f29b1b44236752fa04ca9fc5306f4433
João Pedro Barberino Silva <joao.p.barberino@gmail.com>
2019-05-29 17:07:09 -0300

    Updates

---
+++ precache-manifest.b91784bac1601ca10b46a706d0b31fe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc4bf7b6e9ed30db0c9f14970cfce92",
+    "url": "/ReactUnsplashApi/index.html"
+  },
+  {
+    "revision": "83619d74aef47f8d9bb6",
+    "url": "/ReactUnsplashApi/static/css/main.e5bd199c.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/ReactUnsplashApi/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "83619d74aef47f8d9bb6",
+    "url": "/ReactUnsplashApi/static/js/main.b8c2d454.chunk.js"
+  },
+  {
+    "revision": "f8688a6c7618e02ca608",
+    "url": "/ReactUnsplashApi/static/js/runtime~main.8db99625.js"
+  }
+]);
